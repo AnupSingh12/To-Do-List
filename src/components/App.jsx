b3cc496833ade3349903b8f917cd580928e8b953
AnupSingh12 <anupsingh111.as@gmail.com>
2024-12-20 12:01:29 +0530

    Adding functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import Note from "./Note"
+import Note from "./Note";
+import notes from "../note"
+import CreateArea from "./CreateArea";
+
 
 
 
 function App(){
     return <div>
        <Header/>
-       <Note/>
+       {notes.map(noteItems => 
+       <Note 
+       key={noteItems.key} 
+       title={noteItems.title} 
+       content={noteItems.content} 
+       /> 
+       )}
        <Footer/>
     </div>
 }
